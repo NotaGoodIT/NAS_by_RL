a54b8207caf3bd78c82b0e83933668b82c47237e
NotaGoodIT <117885826+NotaGoodIT@users.noreply.github.com>
2023-06-29 18:27:11 +0800

    Add files via upload

---
+++ DQN_agent.py
@@ -0,0 +1,206 @@
+#可改:memory size (無用)
+#可改:traning方式, 如新經驗立即訓練 , priority
+#可改:reward function
+#可改:learning rate
+
+#DDQN
+#learning rate = 0.01
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+from ENV import NAS
+import random
+import numpy as np
+from keras import Sequential
+from collections import deque
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+from keras.optimizers import Adam
+import time
+
+np.random.seed(0)
+       
+class DQN:
+
+    """ Implementation of deep q learning algorithm """
+
+    def __init__(self, action_space, state_space):
+        self.action_space = action_space
+        self.state_space = state_space
+        self.epsilon = 1
+        self.gamma = .95
+        self.batch_size = 64
+        self.epsilon_min = .01
+        self.epsilon_decay = .97
+        self.learning_rate = 0.01
+        self.memory = deque(maxlen=1000)
+        self.model = self.build_model()
+        self.target_model = self.build_model()
+        self.update_target_model()
+
+    def build_model(self):
+
+        model = Sequential()
+        model.add(Dense(64, input_shape=(self.state_space,), activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+        
+    def update_target_model(self):
+        #copy weights from model to target_model
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+        
+    # def immediate_train(self, state, next_state, reward, action, done):
+        # a = self.model.predict(state)
+        # predict_value = a[0][action]
+        # b = self.model.predict(next_state)
+        # target = self.gamma * np.amax(b[0]) * (1 - done) + reward
+        # error = predict_value - target
+        # b = self.target_model.predict(next_state)
+        # target = self.gamma * np.amax(b[0]) * (1 - done) + reward
+        # target_full = a
+        # target_full[0][action] = target
+        # self.model.fit(state, target_full, epochs=1, verbose=0)
+        # return error
+        
+    def calculate_error(self, state, next_state, reward, action, done):
+        a = self.model.predict(state)
+        predict_value = a[0][action]
+        b = self.model.predict(next_state)
+        target = self.gamma * np.amax(b[0]) * (1 - done) + reward
+        return predict_value - target
+        
+    def act(self, state):           
+        act_values = self.model.predict(state)
+        print(act_values)
+        f = open('Result/result.txt', 'a')
+        f.write("-------------------------------\nact_values:{} \n".format(act_values))
+        f.close()
+        if np.random.rand() <= self.epsilon:
+            print('select randomly')
+            act_random = random.randrange(self.action_space)
+            print('action:',act_random)
+            return act_random  
+        print('select max')
+        act_max = np.argmax(act_values[0])
+        print('action:',act_max)
+        return act_max
+
+    
+    def replay(self):
+        if len(self.memory) < self.batch_size:
+            return
+
+        minibatch = random.sample(self.memory, self.batch_size)
+        states = np.array([i[0] for i in minibatch])
+        actions = np.array([i[1] for i in minibatch])
+        rewards = np.array([i[2] for i in minibatch])
+        next_states = np.array([i[3] for i in minibatch])
+        dones = np.array([i[4] for i in minibatch])
+
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+
+        #targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
+        targets = rewards + self.gamma*(np.amax(self.target_model.predict_on_batch(next_states), axis=1))*(1-dones)
+        targets_full = self.model.predict_on_batch(states)
+
+        ind = np.array([i for i in range(self.batch_size)])
+        targets_full[[ind], [actions]] = targets
+
+        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+def train_dqn(episode):
+    loss = []
+    reward_list=[]
+    structure_num = 4
+    action_space = (structure_num *4) +1
+    state_space = (structure_num *2)
+    steps = 1
+    env = NAS()
+    agent = DQN(action_space, state_space)
+    count = 0
+    iteration_count = 0
+    for e in range(episode+1):
+        
+        last_accuracy = 0
+        score = 0
+        if e ==0:
+            print('-------------Baseline Model------------------')
+            reward, next_state, done, accuracy = env.step(0, e , 0, episode)
+        else:
+            state = next_state
+            state = np.reshape(state, (1, state_space))
+            for i in range(steps):
+                    count += 1
+                    print('-------------------------------\nep = ' , e , ' step = ' , i + 1, '\n')
+                    action = agent.act(state)
+                    reward, next_state, done, accuracy = env.step(action, e , i + 1, episode)
+
+                    iteration_count += 1
+                    # action = agent.act(state, env)
+                    # if i == steps - 1:
+                    # 	reward, next_state, done, accuracy = env.step(action, e + 1, i + 1, episode)
+                        
+                    # else:
+                    #     reward, next_state, done, accuracy = env.step(action, e + 1, i + 1, episode)
+                        
+                    #print("reward: {} ".format(reward))
+                    score = reward
+                    next_state = np.reshape(next_state, (1, state_space))
+                    # if uniformity < 0:
+                    #     last_uniformity = 0
+                    # else:
+                    last_accuracy= accuracy
+                    loss.append(last_accuracy)
+                    reward_list.append(score)
+                    
+                    
+                    
+                    error = agent.calculate_error(state, next_state, reward, action, done)
+                    #error = agent.immediate_train(state, next_state, reward, action, done)
+                    print('error = ' + str(error))
+                    agent.remember(state, action, reward, next_state, done)
+                    state = next_state
+                    agent.replay()
+                    if done:
+                        print("\nepisode: {}/{}, last_accuracy: {}".format(e  , episode, last_accuracy))
+                        env.reset()
+                        break
+                    if i == steps - 1:
+                        print("\nepisode: {}/{}, last_accuracy: {}".format(e , episode, last_accuracy))
+                        break
+            
+            
+            agent.update_target_model()
+    return reward_list, loss, iteration_count
+
+if __name__ == '__main__':
+
+    ep = 100
+    
+    f = open('Result/result.txt', 'a')
+    f.write("Loss History for NAS\n")
+    f.close() 
+    reward_list, loss, iteration_count = train_dqn(ep)
+    plt.plot([i for i in range(iteration_count)], loss)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.savefig('./figure1.png')
+    plt.show()
+    plt.plot([i for i in range(iteration_count)], reward_list)
+    plt.xlabel('Iterations')
+    plt.ylabel('Reward')
+    plt.savefig('./figure2.png')
+    plt.show()
+
+    # plt.plot([i for i in range(ep)], loss2)
+    # plt.xlabel('episodes')
+    # plt.ylabel('score')
+    # plt.savefig('./figure2.png')
+    # plt.show()
---
+++ ENV.py
@@ -0,0 +1,340 @@
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+import numpy as np
+import tensorflow as tf
+from keras.datasets import fashion_mnist, mnist, cifar10
+from keras.utils import np_utils
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten, Dropout
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import time
+from keras.preprocessing.image import ImageDataGenerator
+from pathlib import Path
+from keras import backend as K
+class NAS():
+    def __init__(self):
+            self.done = False
+            self.reward = 0
+            self.acc = 0
+            self.acc_old = 0.85
+            self.acc_max = 0
+            self.reward_max = 0
+            self.structure_layers = 4
+            self.kernal_size = [6 for _ in range(self.structure_layers)]    # 1,3,5,7,9
+            self.filters_num = [32 for _ in range(self.structure_layers)]   # 6~64
+            self.state = [self.kernal_size,self.filters_num]
+
+
+
+    def step(self, action, ep, step, max_ep):
+            
+            if ep ==0:       
+                self.state = [self.kernal_size, self.filters_num]
+                print(self.state)
+                self.reward, accuracy = self.run_NAS(ep, step, max_ep)
+            else:
+                # change kernal_size
+                for k in range(self.structure_layers):
+                    #### change kernal_size ###
+                    if action == k:
+                        self.kernal_size[k] += 2
+                    elif action== (k + self.structure_layers):
+                        self.kernal_size[k] -= 2
+
+                    #### change filter_num ####
+                    elif action== (k + 2*self.structure_layers):
+                        self.filters_num[k] += 1
+                    elif action== (k + 3*self.structure_layers):
+                        self.filters_num[k] -= 1
+                    #### do nothing ###
+                    elif action==(4*self.structure_layers):
+                        self.filters_num[k]=self.filters_num[k]
+                        self.kernal_size[k]=self.kernal_size[k]
+                self.state = [self.kernal_size, self.filters_num]
+                # print(self.kernal_size)
+                # print(self.filters_num)
+                print(self.state)
+                # Boundary check
+                for ele in range(self.structure_layers):
+                    if (self.kernal_size[ele]<1 or self.kernal_size[ele]>9 or self.filters_num[ele]<6 or self.filters_num[ele]>64):
+                        print('Out of range')
+                        self.done = True
+                        break
+                if self.done == True:
+                    self.reward=-20
+                    accuracy=0
+                else:
+                    self.reward, accuracy = self.run_NAS(ep, step, max_ep)
+                     
+
+            return self.reward, self.state, self.done, accuracy
+
+            
+
+    def run_NAS(self, ep, step, max_ep):
+            baseline_list=[]
+            def Build_Model_and_train(kernal_size,filter_num):
+                batchsize = 32
+                epochs_num = 10
+                Dataset = cifar10
+                customized_dataset = True
+                if customized_dataset ==True:
+                    data_dir = 'D:/sclab/LEDA/NAS_by_RL/CLS/train'
+                    data_dir = Path(data_dir)
+                    train_data_gen = ImageDataGenerator(rescale = 1./255,
+                                                        shear_range = 0.2,
+                                                        zoom_range = 0.2,
+                                                        validation_split = 0.2,
+                                                        horizontal_flip=True
+                                                        )
+                    train_dataset = train_data_gen.flow_from_directory(
+                        directory=data_dir,
+                        target_size=(64,64),
+                        batch_size=32,
+                        shuffle=True,
+                        class_mode='categorical',
+                        subset='training'
+                    )
+
+                    test_dataset = train_data_gen.flow_from_directory(
+                        directory=data_dir,
+                        target_size=(64,64),
+                        batch_size=32,
+                        shuffle=True,
+                        class_mode='categorical',
+                        subset='validation'
+                    )
+                # Load data 
+                if Dataset == mnist:
+                    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+                elif Dataset == fashion_mnist:
+                    (X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
+                elif Dataset == cifar10:
+                    (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+
+                # data pre-processing, data_format='channels_last'(避免error)
+                if Dataset == mnist or Dataset == fashion_mnist :
+                    X_train = X_train.reshape(-1, 28, 28, 1)
+                    X_test = X_test.reshape(-1, 28, 28, 1)
+                    Y_train = np_utils.to_categorical(Y_train, 10)
+                    Y_test = np_utils.to_categorical(Y_test, 10)
+                    inputshape = (28, 28, 1)
+                    class_num=10
+                elif Dataset == cifar10 :
+                    X_train = X_train.astype('float32')/255
+                    X_test = X_test.astype('float32')/255
+                    Y_train = np_utils.to_categorical(Y_train, 10)
+                    Y_test = np_utils.to_categorical(Y_test, 10)
+                    inputshape = (32, 32, 3)
+                    class_num=10
+                if customized_dataset == True:
+                    inputshape = (64, 64, 3)
+                    class_num=18
+
+
+                
+
+                #build Model
+                tf.compat.v1.reset_default_graph()
+                K.clear_session()
+                Model= Sequential()
+
+                #layers
+                for l in range(self.structure_layers):
+                    Model.add(Convolution2D(filter_num[l], (kernal_size[l], kernal_size[l]), input_shape = inputshape, padding = "same", data_format='channels_last'))
+                    Model.add(Activation('relu'))
+                    if  l % 2 ==1:
+                        Model.add(MaxPooling2D(pool_size = (2, 2), padding = "same"))
+                    elif l == (self.structure_layers-1) and l%2 == 0:
+                        Model.add(MaxPooling2D(pool_size = (2, 2), padding = "same"))
+                    
+                    
+                # Fully Connected1 
+                Model.add(Flatten())
+                Model.add(Dense(1024))
+                Model.add(Activation('relu'))
+                
+
+                # Fully Connected2 to (10)
+                Model.add(Dense(class_num))
+                Model.add(Activation('softmax'))
+
+
+                # Model Summary
+                print(Model.summary())
+
+
+                # optimizer
+                adam = Adam(learning_rate = 1e-4)
+
+                # compile
+                Model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+                print("-----------------Traning------------------")
+                # Timer
+                start = time.perf_counter()
+                # Train
+                if customized_dataset == True:
+                    history = Model.fit(train_dataset, batch_size=batchsize, epochs=epochs_num,)
+                else:
+                    history = Model.fit(X_train, Y_train, batch_size=batchsize, epochs=epochs_num,)
+
+                # # plt.plot(history.history['loss'])
+                # # plt.title('Model Loss')
+                # # plt.ylabel('loss')
+                # # plt.xlabel('epoch')
+                # # plt.legend(['train'], loc='upper right')
+                # # plt.savefig('./Loss.png')
+                # # plt.show()
+
+                # # plt.plot(history.history['accuracy'])
+                # # plt.title('Model Accuracy')
+                # # plt.ylabel('accuracy')
+                # # plt.xlabel('epoch')
+                # # plt.legend(['train'], loc='upper left')
+                # # plt.savefig('./Acc.png')
+                # # plt.show()
+
+                print('\n---------------Testing-----------------')
+                if customized_dataset==True:
+                    test_loss, test_accuracy = Model.evaluate(test_dataset)
+                else:
+                    test_loss, test_accuracy = Model.evaluate(X_test,Y_test)
+                Time_required = time.perf_counter() - start
+                print('Time required: ',Time_required)
+                print('\ntest loss: ', test_loss)
+                print('\ntest accuracy: ', test_accuracy)
+
+
+
+                # print('\n---------------Saving Model-----------------')
+                # Model.save('./CNN_classfier.h5')
+                
+                return Model, test_loss, test_accuracy , Time_required
+            if ep ==0:
+                f = open('Result/result.txt', 'a')
+                f.write('ep = ' + str(ep)+'\n')
+                baselineModel, baselinetest_loss, baselinetest_accuracy , baselineTime = Build_Model_and_train(self.kernal_size,
+                                                                                                               self.filters_num)
+                self.acc = baselinetest_accuracy
+                f.write("BaselineTime: {}\n".format(baselineTime))
+                f.write("BaselineAccuracy: {}\n".format(self.acc))
+                f.close()
+            elif ep > 0:
+                f = open('Result/result.txt', 'a')
+                f.write('ep = ' + str(ep) + ' step = ' + str(step) + '\n')
+                f.write("kernal size for each layer: {} \n".format(self.state[0]))
+                f.write("filters num for each layer: {} \n".format(self.state[1]))
+                f.close()
+                #讀取BASELINE
+                f = open('Result/result.txt')
+                for line in f.readlines():
+                    if 'BaselineTime' in line:
+                        baseline_list.append(line) 
+                    elif 'BaselineAccuracy' in line:
+                        baseline_list.append(line)
+                f.close()
+                for i in range(len(baseline_list)):
+                    baseline_list[i] = baseline_list[i].split(':')[1]
+                    baseline_list[i] = baseline_list[i].replace('\n','')
+                    baseline_list[i] = baseline_list[i].replace(' ','')
+
+                baseline = [float(i) for i in baseline_list]
+                ###
+                baselineAccuracy = baseline[1]
+                baselineTime = baseline[0]
+                print('baselineAccuracy=',str(baselineAccuracy))
+                print('Baselinetime=',str(baselineTime))
+                ###
+
+                
+                
+                     
+                Model, loss, accuracy, Time  = Build_Model_and_train(self.kernal_size, self.filters_num)
+                
+                #accuracy2,loss_history2 = Benchmarking.Benchmarking('mnist', [0,1], ['circuits'], [15], ['pca8'], circuit='QCNN', cost_fn= 'cross_entropy', binary=False)
+                self.acc = accuracy ##計算reward使用
+                #self.acc = accuracy1 ##計算reward使用
+                self.reward = 100*(self.acc -0.80) - 5*(Time/baselineTime)
+                f = open('Result/result.txt', 'a')
+                f.write(str(loss))
+                f.write("\n")
+                #f.write(str(loss_history2))
+                #f.write("\n")
+                f.write("reward: {} \n".format(self.reward))
+                f.write("accuracy: {}\n".format(self.acc))
+                f.close()
+                print("accuracy: {} ".format(self.acc))
+                
+                
+                print("kernal size for each layer: {} \n".format(self.state[0]))
+                print("filter num for each layer: {} \n".format(self.state[1]))
+                print("reward: {} ".format(self.reward))
+
+                if self.acc > self.acc_max:
+                    self.acc_max = self.acc
+                    f = open('bestuniformity.txt', 'w')
+                    f.write('-------------------------------\nep = ' + str(ep) + ' step = ' + str(step) + '\n')
+                    f.write("kernal size for each layer: {} \n".format(self.state[0]))
+                    f.write("filter num for each layer: {} \n".format(self.state[1]))
+                    f.write("reward: {} \n".format(self.reward))
+                    f.write("accuracy: {} \n".format(self.acc))
+                    # save model
+                    Model.save('./CNN_classfier_bestAcc.h5')
+                if self.reward > self.reward_max:
+                    self.reward_max = self.reward
+                    f = open('bestuniformity.txt', 'w')
+                    f.write('-------------------------------\nep = ' + str(ep) + ' step = ' + str(step) + '\n')
+                    f.write("kernal size for each layer: {} \n".format(self.state[0]))
+                    f.write("filter num for each layer: {} \n".format(self.state[1]))
+                    f.write("reward: {} \n".format(self.reward))
+                    f.write("accuracy: {} \n".format(self.acc))
+                    Model.save('./CNN_classfier_bestReward.h5')
+                del Model
+            self.acc_old = self.acc
+                    
+            return self.reward, self.acc_old
+
+
+    
+
+        
+
+    def reset(self):
+            self.done = False
+            self.reward = 0
+            self.kernal_size = [6 for _ in range(self.structure_layers)]
+            self.filters_num = [32 for _ in range(self.structure_layers)]     
+            self.state = [self.kernal_size,self.filters_num]
+            self.acc_old = 0.65
+
+
+            return self.state
+# def SaveModel(layers_num):
+# # Save Model
+#     print('\n---------------Saving Model-----------------')
+#     Model, loss, accuracy = Build_Model_and_train(layers_num)
+#     Model.save('./CNN_classfier.h5')
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+# # Load Model
+# Model = load_model('./CNN_classfier.h5')
+# print('\n---------------Loading Model-----------------')
+# print(Model.summary())
+
+
+
+
+# one_img_predict(500)  
